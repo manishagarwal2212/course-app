3036fda772619dfd3569761b879503497058ebc9
machau kumar <kumarma2212@gmail.com>
2020-08-30 13:04:16 +0530

    database connect

--- src/main/java/com/edukart/course/app/CourseAppApplication.java
+++ src/main/java/com/edukart/course/app/CourseAppApplication.java
@@ -2,12 +2,18 @@ package com.edukart.course.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CourseAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseAppApplication.class, args);
+	}
+	
+	@GetMapping()
+	String hello() {
+		return "Welcome to content service";
 	}
 
 }
