c04d201259a663ec37ec166af328cedce1e01330
manishagarwal2212 <manishagarwal2212@gmail.com>
2020-09-12 19:35:32 +0530

    mongo db added

--- src/main/java/com/edukart/models/DatabaseSequence.java
+++ src/main/java/com/edukart/models/DatabaseSequence.java
@@ -3,11 +3,7 @@ package com.edukart.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Document(collection = "database_sequences")
 public class DatabaseSequence {
 
