17b51284bc029247e48168fd0594e896981fb178
manishagarwal2212 <manishagarwal2212@gmail.com>
2020-10-02 18:31:13 +0530

    home added for ecomm project

--- src/main/java/com/edukart/CourseAppApplication.java
+++ src/main/java/com/edukart/CourseAppApplication.java
@@ -3,15 +3,18 @@ package com.edukart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CourseAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseAppApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
+	@GetMapping("/")
 	String hello() {
 		return "Welcome to content service";
 	}
